23c63a5474a527fc5375ac1252c8ecb4573f1c12
dalogrus <sburek+github@gmail.com>
2015-06-29 22:38:23 +0200

    third commit

---
+++ ExchangeAGram-SB/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram-SB
+//
+//  Created by Sebastian Burek on 29.06.2015.
+//  Copyright (c) 2015 Sebastian Burek. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
--- ExchangeAGram-SB/FeedViewController.swift
+++ ExchangeAGram-SB/FeedViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -34,4 +34,17 @@ class FeedViewController: UIViewController {
     }
     */
 
+    // UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 }
