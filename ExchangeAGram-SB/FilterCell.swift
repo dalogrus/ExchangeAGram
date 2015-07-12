a165f439c571c29c7139f2cd17dba5aa877f1f7b
dalogrus <sburek@gmail.com>
2015-07-12 15:11:40 +0200

    Implementing FilterCell
    Error - placeholder images are not displaying correctly.

---
+++ ExchangeAGram-SB/FilterCell.swift
@@ -0,0 +1,26 @@
+//
+//  FilterCell.swift
+//  ExchangeAGram-SB
+//
+//  Created by Sebastian Burek on 12.07.2015.
+//  Copyright (c) 2015 Sebastian Burek. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+    var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
