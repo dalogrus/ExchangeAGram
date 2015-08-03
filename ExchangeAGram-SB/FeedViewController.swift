079d71cc46ad584c63deba5165af45d069222d57
dalogrus <sburek+github@gmail.com>
2015-08-03 23:31:28 +0200

    Some assets added

--- ExchangeAGram-SB/FeedViewController.swift
+++ ExchangeAGram-SB/FeedViewController.swift
@@ -22,6 +22,9 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let backgroundImage = UIImage(named: "OceanBackGround")
+        self.view.backgroundColor = UIColor(patternImage: backgroundImage!)
+        
         // Do any additional setup after loading the view.
         
         locationManager = CLLocationManager()
